56f1716cd64ae83b087e0e97c14d49927bdc7a13
Petra-Johansson <petrajohansson@hotmail.com>
2022-10-06 21:46:20 +0200

    deploytest

--- server.js
+++ server.js
@@ -33,7 +33,10 @@ app.use("/api/comments", commentsRoutes);
 const port = process.env.PORT || 8080;
 //database-connection
 mongoose
-  .connect(process.env.MONGO_URI_CLOUD)
+  .connect(process.env.MONGODB_URI || process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }, )
   .then(() => console.log("Connected to database"))
   .catch((err) => console.log(err))
   .then(() => {
